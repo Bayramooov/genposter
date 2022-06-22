38b787a809024c0d08f7992b048690be18c43bbf
Sardor Bayramov <bayramovSardor@gmail.com>
2022-06-22 15:33:45 +0500

    refactoring: lib from JIMP to SHARP

--- index.js
+++ index.js
@@ -1,14 +1,117 @@
-const Jimp = require('jimp') ;
+const sharp = require("sharp");
 
-async function textOverlay() {
-   // Reading image
-   const image = await Jimp.read('./test.jpg');
-   // Defining the text font
-   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-   image.print(font, 10, 350, 'Hello World!');
-   // Writing image after processing
-   await image.writeAsync('./test2.jpg');
+async function getMetadata() {
+  try {
+    const metadata = await sharp("test.jpg").metadata();
+    console.log(metadata);
+  } catch (error) {
+    console.log(`An error occurred during processing: ${error}`);
+  }
 }
 
-textOverlay();
-console.log("Image is processed succesfully");
+async function resizeImage() {
+  try {
+    await sharp("test.jpg")
+      .resize({
+        width: 150,
+        height: 97
+      })
+      .toFile("test-resized.jpg");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function formatImage() {
+  try {
+    // this must be .png format
+    await sharp("test.jpg")
+      .toFormat("jpeg", { mozjpeg: true })
+      .toFile("test-compressed.jpeg");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function cropImage() {
+  try {
+    await sharp("test.jpg")
+      .extract({ width: 500, height: 330, left: 120, top: 70  })
+      .toFile("test-cropped.jpg");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function grayscaleImage() {
+  try {
+    await sharp("test.jpg")
+      .grayscale()
+      .toFile("test-grayscale.jpg");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function rotateImage() {
+  try {
+    await sharp("test.jpg")
+      .rotate(33, { background: { r: 255, g: 255, b: 255, alpha: 1 } })
+      .toFile("test-rotated.jpg");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function blurImage() {
+  try {
+    await sharp("test.jpg")
+      .blur(40)
+      .toFile("test-blurred.jpg");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function textOnImage() {
+  try {
+    const width = 750;
+    const height = 483;
+    const text = "HELLO WORLD";
+
+    const svgImage = `
+    <svg width="${width}" height="${height}">
+      <style>
+        .title {
+          fill: yellow;
+          font-size: 100px;
+          font-weight: bold;
+        }
+      </style>
+      <text x="50%" y="50%" font-family="Sans Serif" text-anchor="middle" class="title">${text}</text>
+    </svg>
+    `;
+    const svgBuffer = Buffer.from(svgImage);
+    const image = await sharp("test.jpg")
+      .composite([
+        {
+          input: svgBuffer,
+          top: 100,
+          left: 100,
+        }
+      ])
+      .toFile("test-text.jpg");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+getMetadata();
+// resizeImage();
+// formatImage();
+// cropImage();
+// grayscaleImage();
+// rotateImage();
+// blurImage();
+textOnImage();
