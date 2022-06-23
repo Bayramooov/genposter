28e702c3ee9a43dcb678beebb9dba55ca0323fbb
Sardor Bayramov <bayramovSardor@gmail.com>
2022-06-23 10:52:41 +0500

    bug fix

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const genPoster = require('./genposter');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '5572244027:AAEpbxTzbbgEHN78-MFGPIBKoHi5I7PyN1M';
+const token = 'My telegram token';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
